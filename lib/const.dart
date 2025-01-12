d2c70fa2ccd3a5b122687e2b600de0b699a71c4b
AhmedElghareebGeex <ahmed.elghareeb@geexar.dev>
2025-01-12 13:38:15 +0200

    Handle ui and test with paypal integrate

---
+++ lib/const.dart
@@ -0,0 +1,6 @@
+abstract class AppConst {
+  static String clientId =
+      'AQcUv3JeU5-y-luBCaN-AKHAJyFwovKNQBABXFxeluXt_QmrwF_5Sc_QuCRxTv0jP3aJ1OKpasl4Yif9';
+  static String secretId =
+      'EAOoLjUemr7qp0NvksSlQG0mrQe8uChACW7qUemie_i3pkrXCosLMX9dOayK3qPvLsL1KRcMSoCGxU--';
+}
